eb2a02f255588a8b8c1285438f5b2306c272d70e
StanislavAerialist <moonlight_stas@mail.ru>
2022-07-14 14:24:14 +0500

    удачный перенос

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Stanislav");
-        System.out.println("14.07.2022");
-    }
-}

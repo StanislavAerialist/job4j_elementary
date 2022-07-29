1dfbe8b9e0090c9ebaade8c5752256b8b0e31e96
StanislavAerialist <moonlight_stas@mail.ru>
2022-07-29 16:35:35 +0500

    1.6. Еженедельная зарплата

---
+++ src/main/java/ru/job4j/condition/WeeklySalary.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class WeeklySalary {
+    public static int calculate(int[] hours) {
+        int sum = 0;
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] > 8 && i >= 5) {
+                sum = sum + 160 + (hours[i] % 8) * 30;
+            } else if (i >= 5) {
+                sum = sum + hours[i] * 20;
+            } else if (hours[i] > 8) {
+                sum = sum + 80 + (hours[i] % 8) * 15;
+            } else {
+                sum = sum + hours[i] * 10;
+            }
+
+        }
+        return sum;
+    }
+}

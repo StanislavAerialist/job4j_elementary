7bf61837104c05fbe0892ce58c63c785215f6b75
StanislavAerialist <moonlight_stas@mail.ru>
2022-08-01 10:22:06 +0500

    1.8. Необычные пары

---
+++ src/main/java/ru/job4j/condition/PairsCharString.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class PairsCharString {
+    public static boolean check(String l, String r) {
+        boolean rsl = false;
+        rsl = (l.isEmpty() && r.isEmpty()) || ((l.charAt(0) == r.charAt(r.length() - 1)) && (l.charAt(l.length() - 1) == r.charAt(0)));
+        return rsl;
+    }
+}
